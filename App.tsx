fb3243ec1d922c7120bee9a58b1765ba3ab14ab6
Danh <nguyenvietdanhnph@gmail.com>
2021-10-01 10:01:13 +0700

    packages and template design

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -18,3 +19,41 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+=======
+import { persistor, store } from '@src/redux/store';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import AppContainer from './src/app/app.container';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import RemotePushController from '@src/services/RemotePushController';
+
+
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <PaperProvider theme={DefaultTheme}>
+              <AppContainer />
+              <RemotePushController />
+            </PaperProvider>
+          </PersistGate>
+        </Provider>
+        <StatusBar />
+      </SafeAreaProvider>
+    );
+  }
+}
+>>>>>>> cdcaa6e (Initialize expo app)
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,40 @@
 module.exports = function(api) {
+<<<<<<< HEAD
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
   };
 };
+=======
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            [
+                "module-resolver",
+                {
+                    "root": ["./src"],
+                    "alias": {
+                        "@assets/*": "./src/assets/*"
+                    }
+                }
+            ],
+            [
+                "module:react-native-dotenv", {
+                    "moduleName": "react-native-dotenv",
+                    "path": ".env",
+                    "blacklist": null,
+                    "whitelist": null,
+                    "safe": false,
+                    "allowUndefined": true
+                }
+            ]
+        ],
+        env: {
+            production: {
+                plugins: ['react-native-paper/babel'],
+            },
+        },
+    };
+};
+>>>>>>> cdcaa6e (Initialize expo app)
